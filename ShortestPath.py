a8624ab6a325363cd006e178188c0eccdd1dbb27
lplpl <758826560@qq.com>
2025-12-13 16:03:05 +0800

    202512131603

--- ShortestPath.py
+++ ShortestPath.py
@@ -9,21 +9,35 @@ import copy
 
 
 def InsertSort(nums: list) -> list:
-    M = copy.copy(nums)
+    M = copy.deepcopy(nums)
     M[0][0] = [[0]]
 
     for i, line in enumerate(M):
         for j, one in enumerate(line):
             print(nums[i][j])
             if i == 0 and j != 0:
-                M[i][j] = M[i][j-1] + [nums[i][j]]
-                print(M)
+                M[i][j] = copy.deepcopy(M[i][j-1])
+                for lastData in M[i][j]:
+                    lastData.append(nums[i][j])
             if j == 0 and i != 0:
-                M[i][j] = M[i-1][j] + [nums[i][j]]
-                print(M)
-            M[i][j] = []
-            M[i][j].append(M[i][j-1] + [nums[i][j]])
-            M[i][j].append(M[i-1][j] + [nums[i][j]])
+                M[i][j] = copy.deepcopy(M[i-1][j])
+                for lastData in M[i][j]:
+                    lastData.append(nums[i][j])
+
+            if i != 0 and j != 0:
+                pathOne = copy.deepcopy(M[i][j-1])
+                print(pathOne)
+                pathTwo = copy.deepcopy(M[i-1][j])
+                print(pathTwo)
+                M[i][j] = []
+                for data in pathOne:
+                    data.append(nums[i][j])
+                    M[i][j].append(data)
+                for data in pathTwo:
+                    data.append(nums[i][j])
+                    M[i][j].append(data)
+            print(M)
+            print("------over------")
 
 
     return 0
